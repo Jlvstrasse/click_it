41862bb7e3942da9ef6f7295373c13049731981c
Jlvstrasse <jlvstrasse@gmail.com>
2024-03-19 18:04:44 -0400

    added Again button

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,12 @@ function changeColors() {
     var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
     document.getElementsByTagName("body")[0].style.background = colors[index++];
+    // document.getElementsByClassName("container")[0].value = "Again";
 
     if (index > colors.length - 1)
         index = 0;
 }
+
+
+    
+
